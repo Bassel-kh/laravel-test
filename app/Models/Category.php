e5232ab814a90bc79e3665df6c68c977f66f97f5
Bassel-kh <bassel.kh.svu@gmail.com>
2022-04-21 01:24:23 -0400

    #3 Manage Categories CRUD: Table, Edit, Create, Delete

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['name'];
 //    protected $table = 'categories'; // by default depends on class name
 }
